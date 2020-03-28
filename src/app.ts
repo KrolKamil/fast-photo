82cfac3e579df5c3090a7884d0ef538cac4a80b2
KrolKamil <kamilkrol1997@wp.pl>
2020-03-28 22:05:51 +0100

    init

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+require('dotenv').config()
+import { Server } from "ws";
+ 
+const wss: Server = new Server({ port: 8080 });
+
+wss.on(('connection'), (ws) => {
+    ws.on('message', (message) => {
+        console.log('received: %s', message);
+    })
+    ws.send('something');   
+});
---
+++ src/handlers/auth/index.js
@@ -0,0 +1,11 @@
+import IMessage from '../../models/interfaces/IMessage';
+
+class Auth {
+  handle = (message: IMessage) => {
+    
+  }
+}
+
+Auth.TYPE = 'auth';
+
+module.exports = Auth;
