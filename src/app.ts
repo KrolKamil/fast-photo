fa410f724578875519d31ce4e4aa5625124f99e2
KrolKamil <kamilkrol1997@wp.pl>
2020-03-29 19:59:00 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,14 @@
 require('dotenv').config()
 import { Server } from "ws";
 import {parseJsonAsync} from './utils';
-import isMessage from './services/validators/message';
-// import Handlers from './handlers';
-// import Auth from './handlers/auth';
-// import IHandler from "./models/interfaces/IHandler";
-// import IHandlers from "./models/interfaces/IHandlers";
-
+import messageCreator from './services/creators/message'
 const wss: Server = new Server({ port: 8080 });
-
-// const coreHandlers = new Map<string, IHandler>();
-// coreHandlers.set(Auth.name.toLowerCase(), new Auth());
-
-// const handlers = new Handlers();
 
 wss.on(('connection'), (ws) => {
     ws.on('message', async (message: string) => {
         try{
             const parsedMessage = await parseJsonAsync(message);
-            if(isMessage(parsedMessage)){
-            }
+            const updatedMessage = messageCreator(parsedMessage);
         } catch (e) {
 
         }
--- src/services/creators/message/index.ts
+++ src/services/creators/message/index.ts
@@ -1,19 +1,34 @@
 import IMessage from "../../../models/interfaces/IMessage"
 
-const getPrefixAndSurfixOfType = {
-    
+const getPrefixAndSurfixOfType = (type: string) => {
+    const indexOfSeparator = type.search('_');
+    if(indexOfSeparator === -1){
+        throw new Error('Type is invalid');
+    }
+    const splitedType: Array<string> = type.split('_');
+    if(splitedType.length > 2){
+        throw new Error('Type is invalid');
+    }
+    return {
+        prefix: splitedType[0],
+        surfix: splitedType[1]
+    }
 };
 
-const message = (message: any) => {
+const message = (message: any): IMessage => {
     if(message){
         if( (message.type) && (message.payload) ){
+            const dividedType = getPrefixAndSurfixOfType(message.type);
             const newMessage: IMessage = {
                 type: message.type,
                 payload: message.payload,
-
+                prefix: dividedType.prefix,
+                surfix: dividedType.surfix
             }
             return newMessage;
         }
     }
     throw new Error('Type and payload are required')
 }
+
+export default message;
