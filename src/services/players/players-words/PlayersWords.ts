a85cfb92364253b54678341fb75ecf08e4cc2f1e
KrolKamil <krol.kamil23@gmail.com>
2020-04-28 20:52:40 +0200

    a

--- src/services/players/players-words/PlayersWords.ts
+++ src/services/players/players-words/PlayersWords.ts
@@ -1,6 +1,7 @@
 import words from '../../Words';
 import players from '../Players';
 import IPlayer from '../../../models/interfaces/IPlayer';
+import eventBus from '../../eventBus';
 
 class PlayersWords {
   private wordsAreSet: boolean;
@@ -18,6 +19,23 @@ class PlayersWords {
       player.word = words.getRandomWord();
       players.edit(player.id, player);
     });
+  };
+
+  public sendWordsToPlayers = (): Promise<void> => {
+    const responses: Array<IResponse>;
+    players.getAll().forEach((player) => {
+      const response: IResponse = {
+        ws: player.ws,
+        message: {
+          type: 'player_word',
+          payload: {
+            word: player.word
+          }
+        }
+      };
+      responses.push(response);
+    });
+    eventBus.next(responses);
   };
 
   getPlayerWord = (playerId: string): string => players.get(playerId).id;
--- src/supervisors/game/Game.ts
+++ src/supervisors/game/Game.ts
@@ -1,9 +1,6 @@
 import stage, { stages } from '../../services/Stage';
 import playersReady from '../../services/players/players-ready/PlayersReady';
 import playersWords from '../../services/players/players-words/PlayersWords';
-import IResponse from '../../models/interfaces/IResponse';
-import players from '../../services/players/Players';
-import eventBus from '../../services/eventBus';
 
 class Game {
   constructor() {
@@ -19,24 +16,7 @@ class Game {
   private start = async (): void => {
     stage.set(stages.GAME);
     playersWords.setPlayersWords();
-    await this.sendWordsToPlayers();
-  };
-
-  sendWordsToPlayers = (): Promise<void> => {
-    const responses: Array<IResponse>;
-    players.getAll().forEach((player) => {
-      const response: IResponse = {
-        ws: player.ws,
-        message: {
-          type: 'player_word',
-          payload: {
-            word: player.word
-          }
-        }
-      };
-      responses.push(response);
-    });
-    eventBus.next(responses);
+    await playersWords.sendWordsToPlayers();
   };
 }
 
