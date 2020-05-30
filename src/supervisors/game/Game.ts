3b1bf13db55b2d62b107dc3c22321c5eae937d81
KrolKamil <kamilkrol1997@wp.pl>
2020-05-30 18:40:29 +0200

    game reset!

--- src/supervisors/game/Game.ts
+++ src/supervisors/game/Game.ts
@@ -1,16 +1,44 @@
 import stage, { stages } from '../../services/Stage';
 import { resetSocket } from '../../utils';
 import players from '../../services/players/Players';
-import TPlayers from '../../models/types/TPlayers';
+import playersActive from '../../services/players/players-active/PlayersActive';
 
 class Game {
+  private gameCancelTimeout: NodeJS.Timeout | null;
   constructor() {
+    this.gameCancelTimeout = null;
     stage.observe().subscribe(this.handleGameEnd);
     players.observe().subscribe(this.handleGameCancel);
   }
 
-  private handleGameCancel = (currentPlayers: TPlayers) => {
+  private handleGameCancel = (): void => {
     if (stage.current() === stages.GAME) {
+      if (
+        playersActive.areAllPlayersInactive() &&
+        this.gameCancelTimeout === null
+      ) {
+        this.setGameCancelTimeout();
+      } else if (
+        !playersActive.areAllPlayersInactive() &&
+        this.gameCancelTimeout !== null
+      ) {
+        this.unsetGameCancelTimeout();
+      }
+    }
+  };
+
+  private gameCancel = (): void => {
+    resetSocket();
+  };
+
+  private setGameCancelTimeout = (): void => {
+    this.gameCancelTimeout = setTimeout(this.gameCancel, 15000);
+  };
+
+  private unsetGameCancelTimeout = (): void => {
+    if (this.gameCancelTimeout !== null) {
+      clearTimeout(this.gameCancelTimeout);
+      this.gameCancelTimeout = null;
     }
   };
 
