c1b5e0f524292fa2d83be287bb939f31c508b7a6
KrolKamil <krol.kamil23@gmail.com>
2020-06-04 23:05:18 +0200

    more enitites

--- src/services/Words.ts
+++ src/services/Words.ts
@@ -2,7 +2,16 @@ class Words {
   words: Array<string>;
   constructor() {
     // this.words = ['Mouse', 'Person']
-    this.words = ['Person'];
+    this.words = [
+      'Person',
+      'Car',
+      'Pen',
+      'Chair',
+      'Mouse',
+      'Cup',
+      'Pillow',
+      'Key',
+      'Book'];
   }
 
   getRandomWord = (): string => {
